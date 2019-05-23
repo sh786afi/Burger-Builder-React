eafa8bb0abc3bd7c38d9170bc6c1a6da687644e8
shafi akhtar <sh786afi@gmail.com>
2019-05-23 17:38:49 +0530

    button componentn

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -15,6 +15,8 @@ const orderSummary = props => {
       <p>A Delecius Burger With The Following Ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to checkout ?</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Aux>
   );
 };
